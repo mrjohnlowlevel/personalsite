16e1adb2e4768568885ff3c310dbd0a2019ce2f5
mrjohnlowlevel <johnfree751@gmail.com>
2026-03-22 16:18:04 +0800

    pls deploy

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/mrjohnlowlevel.github.io/personalsite/',
+  base: '/personalsite/',
   plugins: [
     svelte(),
     tailwindcss()

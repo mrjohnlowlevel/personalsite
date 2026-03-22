3b148927c05df0b6dbebbc3385dc0fe8c3b5b5c7
mrjohnlowlevel <johnfree751@gmail.com>
2026-03-22 14:29:39 +0800

    added small intro

--- src/App.svelte
+++ src/App.svelte
@@ -1,3 +1,14 @@
-  <h1 class="text-3xl font-bold underline text-red-300">
-    Hello world!
+<div id="main">
+  <h1 class="text-4xl text-center bg-red-400 border border-red-200">
+    Hello, World!
   </h1>
+  <br />
+  <div id="information" class="grid grid-cols-2">
+    <p class="">My name is Wunderbar</p>
+    <img
+      alt="Forever Young 2025 Breeders' Cup Classic"
+      src="/ebayanvscousinanddude.jpeg"
+      class="rounded"
+    />
+  </div>
+</div>

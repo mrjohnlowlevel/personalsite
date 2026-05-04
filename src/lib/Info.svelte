9874469d3455470d282d5a148377d026495a5102
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-04 20:54:29 +0800

    added new images

--- src/lib/Info.svelte
+++ src/lib/Info.svelte
@@ -1,5 +1,9 @@
-<script>
+<script lang="ts">
     import { fade } from "svelte/transition";
+
+    const getRandomNum = (max: number): number => {
+        return Math.floor(Math.random() * max);
+    }
 
     const gifs = [
         "https://media0.giphy.com/media/v1.Y2lkPTc5MGI3NjExZW1mdmJwN2RjeWljaDRwY2plN3pnZDJubXA0emtubWdsdWozNmozdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/Dwo8ReUwBZpLekoyWl/giphy.gif",
@@ -9,9 +13,12 @@
         "https://media4.giphy.com/media/v1.Y2lkPTc5MGI3NjExcmR2b2N0ajBma3FxOXBvYzVqYmF6emxsZWhxNzhhMWZsd3V0ZWMzdyZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/1IxfBx0jyAnK3z6Ots/giphy.gif",
     ];
 
-    const randomNumber = Math.floor(Math.random() * gifs.length);
 
-    let selected_gif = gifs[randomNumber];
+    const randomGifNumber = getRandomNum(gifs.length)
+    const ebayan_real_or_not = getRandomNum(2) // state: 1, IRL race; 2 Uma
+    console.log(ebayan_real_or_not)
+
+    let selected_gif = gifs[randomGifNumber];
 </script>
 
 <div id="information">
@@ -26,20 +33,33 @@
         <a href="https://en.wikipedia.org/wiki/Chrono_Genesis" target="_blank" rel="noreferrer noopener">
             <img
                 src={selected_gif}
-                loading="eager"
+                loading="lazy"
                 alt="Random GIF"
                 class="rounded shadow shadow-black hover:scale-105 transition-transform duration-300"
             />
         </a>
 
-        <a href="https://www.racingpost.com/results/444/del-mar/2025-11-01/906888" target="_blank" rel="noreferrer noopener">
-            <img
-                alt="Forever Young 2025 Breeders' Cup Classic"
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSElPd6htVM-0usm89ntbBQhW74qpmSN71nNA&s"
-                loading="eager"
-                class="rounded shadow shadow-black hover:scale-105 transition-transform duration-300"
-            />
-        </a>
+        {#if ebayan_real_or_not == 1}
+            <a href="https://www.racingpost.com/results/444/del-mar/2025-11-01/906888" target="_blank" rel="noreferrer noopener">
+                <img
+                    alt="Forever Young 2025 Breeders' Cup Classic"
+                    src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSElPd6htVM-0usm89ntbBQhW74qpmSN71nNA&s"
+                    loading="lazy"
+                    class="rounded shadow shadow-black hover:scale-105 transition-transform duration-300"
+                />
+            </a>
+        {:else}
+            <a href="https://umamusu.wiki/Forever_Young" target="_blank" rel="noreferrer noopener">
+                <img
+                    alt="Forever Young (Uma Musume) wiki page フォーエバーヤング"
+                    src="/personalsite/Forever_Young.jpeg"
+                    loading="lazy"
+                    height="260px"
+                    width="260px"
+                    class="rounded shadow shadow-black hover:scale-105 transition-transform duration-300"
+                />
+            </a>
+        {/if}
     </div>
 
     <div id="background" class="bg-[#7a91b0] shadow shadow-black">

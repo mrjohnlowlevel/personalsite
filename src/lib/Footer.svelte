e45494652ba57c4a80a40fb37838fb34888adfa4
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-05 22:49:12 +0800

    removed discord link (server deleted)

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,13 +1,6 @@
 <div class="bg-[#222936] border-2 border-cyan-900">
     <h1 class="pt-2 pl-2 text-2xl font-mono uppercase text-white">My socials</h1>
     <ul class="ml-2">
-        <li
-            class="text-blue-500 transition-colors duration-200 hover:text-blue-200"
-        >
-            <a href="https://discord.gg/m9dheqW84T" target="_blank" rel="noreferrer noopener"
-                >▷ Discord server: Fizz Pop</a
-            >
-        </li>
 
         <li
             class="text-gray-400 transition-colors duration-200 hover:text-green-200"

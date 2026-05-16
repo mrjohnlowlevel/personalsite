b837e401d46d9ba9fa6512a4269a542d752fc0b4
mrjohnlowlevel <johnfree751@gmail.com>
2026-05-16 12:51:19 +0800

    added dc user

--- src/lib/Footer.svelte
+++ src/lib/Footer.svelte
@@ -1,17 +1,38 @@
-<div class="bg-[#6f0016]">
-    <div class="bg-crimson p-4"></div>
-    <h1 class="pt-2 pl-2 text-2xl font-mono uppercase text-white">My socials</h1>
-    <ul class="ml-2">
+<script lang="ts">
+    const discord_user: string = "ushba_tesoro";
+</script>
 
-        <li
-            class="text-gray-400 transition-colors duration-200 hover:text-green-200"
-        >
-            <a href="https://github.com/mrjohnlowlevel" target="_blank" rel="noreferrer noopener"
-                >▷ Github: mrjohnlowlevel</a
+<div class="bg-crimson p-4 rounded-t"></div>
+<div class="bg-[#6f0016]">
+    <h1 class="pt-2 pl-2 text-2xl font-mono uppercase text-white">
+        My socials
+    </h1>
+    <ul class="ml-2">
+        <li>
+            <a
+                href="https://github.com/mrjohnlowlevel"
+                target="_blank"
+                rel="noreferrer noopener"
+                ><span
+                    class="text-gray-400 transition-colors duration-200 hover:text-green-200"
+                    >▷ Github: mrjohnlowlevel</span
+                >
+            </a>
+            <p
+                id="dc"
+                class="text-blue-800 transition-colors duration-200 hover:text-green-200"
             >
+                ▷ Discord: {discord_user}
+            </p>
         </li>
     </ul>
 
-    <br>
-    <a href="https://svelte.dev/" target="_blank" rel="noreferrer noopener"><p class="font-extralight transition-colors duration-200 hover:text-orange-500">Made with Svelte (because I'm not a coward)</p></a>
+    <br />
+    <a href="https://svelte.dev/" target="_blank" rel="noreferrer noopener"
+        ><p
+            class="font-extralight transition-colors duration-200 hover:text-orange-500"
+        >
+            Made with Svelte (because I'm not a coward)
+        </p></a
+    >
 </div>
